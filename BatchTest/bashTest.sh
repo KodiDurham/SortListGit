fa0a59965151e03bf2901bbde0dbc233404c428e
KodiD <40518298+KodiD@users.noreply.github.com>
2019-11-12 20:13:43 -0500

    trying to fix bash

--- BatchTest/bashTest.sh
+++ BatchTest/bashTest.sh
@@ -1,7 +1,7 @@
 java -jar CSC499_HW1.jar ascend
-results1=$(diff output.txt expectedUp.txt)
+results1=(diff output.txt expectedUp.txt)
 
 java -jar CSC499_HW1.jar descend
-results2=$(diff output.txt expectedDown.txt)
+results2=(diff output.txt expectedDown.txt)
 
 exit results1 && results2
