2856cf4d06f0a2d49003fcb25d9d712127f778c1
KodiD <40518298+KodiD@users.noreply.github.com>
2019-11-12 20:22:01 -0500

    fix3

--- BatchTest/bashTest.sh
+++ BatchTest/bashTest.sh
@@ -4,4 +4,9 @@ results1=(comm output.txt expectedUp.txt)
 java -jar CSC499_HW1.jar descend
 results2=(comm output.txt expectedDown.txt)
 
+if(results1==1 && results2 ==1);
+    exit 1
+else
+    exit 1
+
 exit results1 && results2
