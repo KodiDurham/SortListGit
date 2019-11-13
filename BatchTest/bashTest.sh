e3a3cfa823aa8e2e331551777c641a78b2f92cc7
KodiD <40518298+KodiD@users.noreply.github.com>
2019-11-12 20:17:46 -0500

    fix 2

--- BatchTest/bashTest.sh
+++ BatchTest/bashTest.sh
@@ -1,7 +1,7 @@
 java -jar CSC499_HW1.jar ascend
-results1=(diff output.txt expectedUp.txt)
+results1=(comm output.txt expectedUp.txt)
 
 java -jar CSC499_HW1.jar descend
-results2=(diff output.txt expectedDown.txt)
+results2=(comm output.txt expectedDown.txt)
 
 exit results1 && results2
